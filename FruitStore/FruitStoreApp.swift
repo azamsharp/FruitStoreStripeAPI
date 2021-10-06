00c22bce20534a09789671f341de8af70dd3b3b3
Mohammad Azam <azamsharp@gmail.com>
2021-10-06 18:05:54 -0500

    Initial Commit

---
+++ FruitStore/FruitStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitStoreApp.swift
+//  FruitStore
+//
+//  Created by Mohammad Azam on 10/6/21.
+//
+
+import SwiftUI
+
+@main
+struct FruitStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
